438c83b3d4bb21e7ce16e549639179e521fe6f67
Susan Chambers <chamberssu@gmail.com>
2021-05-30 15:27:40 -0500

    Added magnitude colors and radius

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,67 +1,112 @@
-// Store our API endpoint as queryUrl.
-var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+//  Create our map, giving it the streetmap and earthquakes layers to display on load.
+  var myMap = L.map("mapid", {
+    center: [37.09, -95.71],
+    zoom: 5
+  });
 
-// Perform a GET request to the query URL/
-d3.json(queryUrl).then(function (data) {
-  // Once we get a response, send the data.features object to the createFeatures function.
-  createFeatures(data.features);
+  // var myMap = L.map('mapid').setView([37.09, -95.71], 5);
+
+  var quakemap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
 
-function createFeatures(earthquakeData) {
 
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2Nod2FtYiIsImEiOiJja3BibTg0NzAwejFzMnVtdTdtbHl5Zzg4In0.-NRiPFnJSHZB87yEzFroTQ', {
+//     maxZoom: 18,
+//     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+//                  '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+//                  'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//     id: 'mapbox.streets'
+//   }).addTo(myMap);
+
+quakemap.addTo(myMap);
+
+// Store our API endpoint as queryUrl.
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
+d3.json(queryUrl).then(function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  function mapFeatures(feature) {
+    return{
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: magColor(feature.properties.mag),
+      color: "#000000",
+      radius: magRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+// Example code below found at: https://www.tutorialspoint.com/can-we-have-a-return-statement-in-a-javascript-switch-statement
+//   function returnDay(val) {
+//     switch (val) {
+//        case 1:
+//           return "It's monday";
+//        case 2:
+//           return "It's tuesday";
+//        case 3:
+//           return "It's wednesday";
+//        case 4:
+//           return "It's thursday";
+//        case 5:
+//           return "It's friday";
+//        case 6:
+//           return "It's saturday";
+//        case 7:
+//           return "It's sunday";
+//        default:
+//           return "Enter a value between 1 - 7";
+//     }
+//  }
+  function magColor(magnitude){
+    switch (true) {
+      case magnitude >5:
+        return "#C50000";
+      case magnitude >4:
+        return "#FF401C";
+      case magnitude >3:
+        return "#FEA83A";
+      case magnitude >2:
+        return "#FCF064";
+      case magnitude >1:
+        return "#B6FC64";
+      default:
+        return "#64FC79"
+    }
   }
 
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
+  function magRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 5;
+  }
 
-  // Send our earthquakes layer to the createMap function/
-  createMap(earthquakes);
-}
+  L.geoJson(data, {
+    pointToLayer: function(feature, latlng) {
+        return new L.CircleMarker(latlng);
+      },
+        style: mapFeatures,
 
-function createMap(earthquakes) {
+    onEachFeature: function(feature, layer) {
+       layer.bindPopup(`<h2>${feature.properties.place}</h2><hr><h2>Magnitude: ${feature.properties.mag}</h2>`)
+       console.log(feature.properties.place)
+       console.log(feature.properties.mag)
+  
+    }
 
-  // Create the base layers.
-  var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
+}).addTo(myMap)
+});
 
-  var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
 
-  // Create a baseMaps object.
-  var baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
-  };
-
-  // Create an overlay object to hold our overlay.
-  var overlayMaps = {
-    Earthquakes: earthquakes
-  };
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  var myMap = L.map("mapid", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [street, earthquakes]
-  });
-
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-
-}
+// Loop through the cities array, and create one marker for each city object.
+// for (var i = 0; i < cities.length; i++) {
+//   L.circle(cities[i].location, {
+//     fillOpacity: 0.75,
+//     color: "white",
+//     fillColor: "purple",
+//     // Setting our circle's radius to equal the output of our markerSize() function:
+//     // This will make our marker's size proportionate to its population.
+//     radius: markerSize(cities[i].population)
+//   }).bindPopup("<h1>" + cities[i].name + "</h1> <hr> <h3>Population: " + cities[i].population + "</h3>").addTo(myMap);
+// }
